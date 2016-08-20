4ada0fce93e1f7c2f289ecb3bdb2d1efe881070f
Jason P-R <jasonpr@Jasons-MacBook-Pro.local>
2016-08-20 17:39:35 -0400

    Add Logger.
    Logger will help us perform assertions about the work fixed_point
    does.  In particular, Logger will help us check that fixed_poing
    doesn't re-evaluate nodes more often than it needs to.
    
    It also will help with debugging.

--- fixed_point.py
+++ fixed_point.py
@@ -1,38 +1,72 @@
 import collections
 
-def log(msg):
-  if False:
-    print msg
+def fixed_point(zero_factory, logger=None):
+  log = logger if logger else NopLogger()
+  return lambda inner_func: _decorator_instance(inner_func, zero_factory, log)
 
-def fixed_point(zero_factory):
-    return lambda inner_func: _decorator_instance(inner_func, zero_factory)
-
-def _decorator_instance(inner_func, zero_factory):
+def _decorator_instance(inner_func, zero_factory, logger):
   quiet = set()
   callers_of = collections.defaultdict(set)
   cached_values = collections.defaultdict(zero_factory)
 
   def modified_func(arg):
     if arg in quiet:
-      log('Cached f(%s) = %s' % (arg, cached_values[arg]))
-      return cached_values[arg]
+      cached_result = cached_values[arg]
+      logger.cached_result(arg, cached_result)
+      return cached_result
     # Claim it is quiet just before calling the inner_func (and risking recursion).
     # This way, if a cycle occurs, it execution will terminate.
     quiet.add(arg)
 
     def recurse(child):
       """Note that the current caller depends on the child, then recurse normally."""
-      log('%s calls %s' % (arg, child))
+      logger.recursed(arg, child)
       callers_of[child].add(arg)
       return modified_func(child)
 
     # Keep on chasing quietness around until it settles everywhere.
+    logger.computing(arg)
     new_result = inner_func(arg, recurse)
+    logger.computed(arg, new_result)
     while new_result != cached_values[arg]:
       cached_values[arg] = new_result
-      # When a node's value changes, its dependors' values are called into doubt.
+      # When a node's value changes, call into doubt the value of everyone who depends on it.
       quiet.difference_update(callers_of[arg])
-      log("Computing %s" % arg)
+      logger.computing(arg)
       new_result = inner_func(arg, recurse)
+      logger.computed(arg, new_result)
     return new_result
   return modified_func
+
+class _AbstractLogger(object):
+
+  def cached_result(self, arg, value):
+    self._log('CACHED_RESULT', (arg, value))
+
+  def recursed(self, parent, child):
+    self._log('RECURSED', (parent, child))
+
+  def computing(self, arg):
+    self._log('COMPUTING', arg)
+
+  def computed(self, arg, value):
+    self._log('COMPUTED', (arg, value))
+
+class Logger(_AbstractLogger):
+  def __init__(self):
+    self.log = []
+
+  def _log(self, event_type, details):
+    self.log.append((event_type, details))
+
+  def clear(self):
+    self.log = []
+
+  def pretty_print(self):
+    for entry in self.log:
+      print '%s: %s' % entry
+
+class NopLogger(_AbstractLogger):
+
+  def _log(self, event_type, details):
+    pass
